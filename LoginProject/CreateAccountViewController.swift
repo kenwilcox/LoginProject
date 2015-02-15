b31d187a5d8dde0c738cd22481ddbb65b55527e4
Ken Wilcox <wilcoxk@gmail.com>
2015-02-15 12:16:31 -0700

    Added UI Elements

--- LoginProject/CreateAccountViewController.swift
+++ LoginProject/CreateAccountViewController.swift
@@ -9,27 +9,25 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  @IBOutlet weak var chooseUsernameTextField: UITextField!
+  @IBOutlet weak var choosePasswordTextField: UITextField!
+  @IBOutlet weak var confirmPasswordTextField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func createAccountButtonPressed(sender: UIButton) {
+  }
+  
+  @IBAction func cancelButtonPressed(sender: UIButton) {
+  }
 }
