62219316848457d52374b5f8f09b4743fbc97285
Ken Wilcox <wilcoxk@gmail.com>
2015-02-15 13:21:07 -0700

    implement CreateAccountViewControllerDelegate

--- LoginProject/LoginViewController.swift
+++ LoginProject/LoginViewController.swift
@@ -24,11 +24,25 @@ class LoginViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "loginToCreateAccountSegue" {
+      var createAccountVC = segue.destinationViewController as CreateAccountViewController
+      createAccountVC.delegate = self
+    }
+  }
+  
   @IBAction func loginButtonPressed(sender: UIButton) {
     self.performSegueWithIdentifier("loginToMainSegue", sender: self)
   }
   
   @IBAction func createAccountButtonPressed(sender: UIButton) {
     self.performSegueWithIdentifier("loginToCreateAccountSegue", sender: self)
+  }
+}
+
+// MARK: CreatedAccountViewControllerDelegate
+extension LoginViewController: CreateAccountViewControllerDelegate {
+  func accountCreated() {
+    self.performSegueWithIdentifier("loginToMainSegue", sender: nil)
   }
 }
